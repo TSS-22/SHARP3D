deb10cd9697f6b0efe110a55bb609feed1cbeaac
TSS-22 <64001864+TSS-22@users.noreply.github.com>
2026-02-09 12:30:22 +1300

    Working on constructor. Gonna discrd C3dFileManager

--- SHARP3D/SHARP3D/C3dFile.cs
+++ SHARP3D/SHARP3D/C3dFile.cs
@@ -6,12 +6,26 @@
     /// </summary>
     public class C3dFile
     {
-        ProcessorType processorType;
+        /// <summary>
+        /// Represents the file stream used to access the C3D file.
+        /// </summary>
+        FileStream fileStream { get; set; } = null;
+        /// <summary>
+        /// Specifies the type of processor that was used to create the C3D file.
+        /// </summary>
+        ProcessorType processorType { get; set; } = BitConverter.IsLittleEndian? ProcessorType.INTEL : ProcessorType.SIG_MIPS;
         // TODO: Implement constructor logic.
-        public C3dFile(byte[] c3dBinairies)
-        {
-            //C3dFileManager.ReadProcessorByte(c3dBinairies);
+        private C3dFile(){ }
 
+        private C3dFile(FileStream fileStream, ProcessorType processorType)
+        {
+            this.fileStream = fileStream;
+            this.processorType = processorType;
+        }
+
+        public C3dFile CreateEmpty()
+        {
+            return new C3dFile();
         }
 
         // TODO: Implement actual header processing logic.
@@ -29,7 +43,9 @@
         // TODO: Implement actual file loading logic. Use the C3dFileManager for this
         public static C3dFile LoadFromFile(string filepath)
         {
-            return new C3dFile(new byte[] { });
+            FileStream fileStream = OpenC3dFile(filepath);
+            ProcessorType processorType = ReadProcessorByte(fileStream);
+            return new C3dFile(fileStream, processorType);
         }
 
         // TODO: Implement actual binaries transformation logic.
@@ -44,6 +60,32 @@
             return 0;
         }
 
+        public static ProcessorType ReadProcessorByte(FileStream c3dStream)
+        {
+            int parameterSectionPointer = GetParameterSectionPointer(c3dStream);
+            c3dStream.Seek(parameterSectionPointer + 3, SeekOrigin.Begin);
+            return (ProcessorType)c3dStream.ReadByte();
+        }
+
+        public static int GetParameterSectionPointer(FileStream c3dStream)
+        {
+            byte[] pointerToParameter = new byte[1];
+            c3dStream.Seek(0, SeekOrigin.Begin);
+            c3dStream.ReadExactly(pointerToParameter, 0, 1);
+            return BitConverter.ToInt16(new byte[] { 0, pointerToParameter[0] }, 0);
+        }
+
+        public static byte[] ReadHeader(FileStream c3dStream)
+        {
+            byte[] headers = new byte[512];
+            c3dStream.ReadExactly(headers, 0, 512);
+            return headers;
+        }
+
+        public static FileStream OpenC3dFile(string filepath)
+        {
+            return new FileStream(filepath, FileMode.Open, FileAccess.Read);
+        }
 
     }
 }
--- SHARP3D/SHARP3D/logic/C3dFileManager.cs
+++ SHARP3D/SHARP3D/logic/C3dFileManager.cs
@@ -5,28 +5,14 @@ namespace SHARP3D
     public static class C3dFileManager
     {
 
-        public static byte[] ReadHeader(FileStream c3dStream)
-        {
-            byte[] headers = new byte[512];
-            c3dStream.ReadExactly(headers, 0, 512);
-            return headers;
-        }
+        
 
-        public static int GetParameterSectionPointer(FileStream c3dStream)
-        {
-            byte[] pointerToParameter = new byte[1];
-            c3dStream.Seek(0, SeekOrigin.Begin);
-            c3dStream.ReadExactly(pointerToParameter, 0, 1);
-            return BitConverter.ToInt16(new byte[] { 0, pointerToParameter[0]}, 0);
-        }
+        
+
+        
 
 
-        public static ProcessorType ReadProcessorByte(FileStream c3dStream)
-        {
-            int parameterSectionPointer = GetParameterSectionPointer(c3dStream);
-            c3dStream.Seek(parameterSectionPointer + 3, SeekOrigin.Begin);
-            return (ProcessorType)c3dStream.ReadByte();
-        }
+        
 
     }
 }
