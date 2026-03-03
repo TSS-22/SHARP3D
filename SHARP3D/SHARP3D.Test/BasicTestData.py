a458a9a35d818fe58b310500d9ca7ad34058c532
TSS-22 <64001864+TSS-22@users.noreply.github.com>
2026-03-03 15:19:14 +1300

    Added TestFiles and BasicData retrieves scripts

---
+++ SHARP3D/SHARP3D.Test/BasicTestData.py
@@ -0,0 +1,53 @@
+import glob
+import os
+from ezc3d import c3d
+import json
+import sys
+
+
+c3d_files = glob.glob(f"{sys.argv[1]}/*.c3d")
+
+
+for path in c3d_files:
+    filename = os.path.splitext(os.path.basename(path))[0]
+    foldername = os.path.splitext(os.path.dirname(path))[0]
+    try:
+        c = c3d(path)
+        
+        groups_parameter = list(c['parameters'].keys())
+
+        parameters = list()
+
+        for group in groups_parameter:
+            if group == "__METADATA__":
+                continue
+            parameters.append([parameter for parameter in c['parameters'][group] if parameter != "__METADATA__"])
+
+        point_first_0 = c['data']['points'][:3,0,0].tolist()
+        point_last_0 = c['data']['points'][:3,0,-1].tolist()
+
+        analog_first_0 = c['data']['analogs'][0,0,0]
+        analog_last_0 = c['data']['analogs'][0,0,-1]
+
+        point_frames = c['data']['points'].shape[-1]
+        analog_frames = c['data']['analogs'].shape[-1]
+
+
+        data ={
+            "groups_parameter": groups_parameter,
+            "parameters": parameters,
+            "point_first_0": point_first_0,
+            "point_last_0": point_last_0,
+            "analog_first_0": analog_first_0,
+            "analog_last_0": analog_last_0,
+            "point_frames": point_frames,
+            "analog_frames": analog_frames
+        }
+
+
+        with open(f"{foldername}/{filename}.json", "w") as f:
+            json.dump(data, f, indent=4)
+            
+    except Exception as e:
+        with open("./exceptions.txt", "a") as file:
+            file.write(f"{foldername}/{filename}.c3d: {e}\n")
---
+++ SHARP3D/SHARP3D.Test/RunTestDataScripts.py
@@ -0,0 +1,20 @@
+import os
+import subprocess
+
+# Path to the script you want to run in each subfolder
+basic_data_script = "BasicTestData.py"  # Replace with your script name
+
+# Path to the parent folder
+parent_folder = "./TestFiles"  # Replace with your folder path
+
+# Create a file to store the exceptions and the reason
+with open("exceptions.txt", "w"):
+    pass
+
+for root, dirs, files in os.walk(parent_folder):
+    for subfolder in dirs:
+        subfolder_path = os.path.join(root, subfolder)
+        print(f"Running basic data script in: {subfolder_path}")
+
+        # Run the script in the subfolder
+        subprocess.run(["python", basic_data_script, subfolder_path])
