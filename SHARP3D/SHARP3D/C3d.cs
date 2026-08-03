c6ab67f838cbe9647c9c4c1f425cd53389b084a1
TSS-22 <64001864+TSS-22@users.noreply.github.com>
2026-08-03 15:55:38 +1200

    Ready to work on data to binary

--- SHARP3D/SHARP3D/C3d.cs
+++ SHARP3D/SHARP3D/C3d.cs
@@ -3,13 +3,8 @@ using SHARP3D.Header.DataEntity;
 using SHARP3D.Parameter.DataEntity.Clean;
 using SHARP3D.Utils;
 using SHARP3D.Utils.Enum;
-using System.Collections.Generic;
-using System.Data.Common;
-using System.Net.Sockets;
-using System.Reflection.Metadata;
-using System.Reflection.Metadata.Ecma335;
-using System.Xml.Linq;
-using static System.Net.Mime.MediaTypeNames;
+using System.Reflection.PortableExecutable;
+
 
 namespace SHARP3D
 {
@@ -130,12 +125,151 @@ namespace SHARP3D
             return Data.Points.Length != 0 ? Data.Points[0].Residual.Length : 0;
         }
 
-        public void Save(string filePath, FileFrameFormat fileFrameFormat = FileFrameFormat.C3D_STANDARD)
+        public void Save(string filePath, string fileName, FileFrameFormat fileFrameFormat = FileFrameFormat.C3D_STANDARD)
         {
+            var fs = new FileStream(
+                $"{filePath}/{fileName}.c3d",
+                FileMode.OpenOrCreate,     // Create if doesn't exist
+                FileAccess.Write,
+                FileShare.None,
+                bufferSize: 4096,
+                FileOptions.Asynchronous   // Enable async
+            );
+            fs.Seek(0, SeekOrigin.Begin);
+
             byte[] parameters = ParametersToBinaries();
-            byte[] header = HeaderToBinaries(parameters.Length / 512);
-            //byte[] data = DataToBinaries();
-            //byte[] c3dBinaries = header.Concat(parameters).Concat(data).ToArray();
+            byte[] header = HeaderToBinaries((parameters.Length / 512)); // We add the +1 in the HeaderToBinaries function
+
+            fs.WriteAsync(header);
+            fs.WriteAsync(parameters);
+
+            // Compute scale factor.
+            // This is another reason to put the computation of this managed parameter and their byte function into their respective class
+            // To keep a single source of truth.
+            float maximumPointValue = 0;
+            foreach (C3dPointTrajectory trajectory in Data.Points)
+            {
+                foreach (float? val in trajectory.Point)
+                {
+                    if (val == null)
+                    {
+                        continue;
+                    }
+                    else if (Math.Abs((float)val) > maximumPointValue)
+                    {
+                        maximumPointValue = (float)val;
+                    }
+                }
+            }
+            byte[] data = DataToBinaries(
+                maximumPointValue/32000,
+                fs
+                );
+            fs.Close();
+        }
+
+        // This function add the +1 block from the parameter length to compute blocks to data start.
+        public byte[] HeaderToBinaries(int blockLengthParameterSection)
+        {
+            List<byte> header = new List<byte>();
+            //   Byte 1: uint8, Byte 2: char Byte 1: Number of 512 - byte blocks to Parameter Section +1.
+            header.Add((byte)0x02);
+            //    Byte 2: Data storage format flag.
+            header.Add((byte)0x50);
+            //2   uint16 Number of markers stored in each Data Frame.
+            header.AddRange(BitConverter.GetBytes((UInt16)Data.Points.Length));
+            //3   uint16 Total number of analog samples per Data Frame.
+            header.AddRange(BitConverter.GetBytes((UInt16)(Required.Analog.SamplesPerFrame * Data.Analogs.Length)));
+            //4   uint16 First frame number of raw data(not used / misleading).
+            header.AddRange(BitConverter.GetBytes((UInt16)1));
+            //5   uint16 Last frame number of raw data(not used / misleading).
+            header.AddRange(BitConverter.GetBytes((UInt16)Required.Point.Frames));
+            //6   uint16 Maximum 3D frame interpolation gap.
+            header.AddRange(BitConverter.GetBytes((UInt16)Required.Point.MaximumInterpolationGap));
+            //7 - 8     float32 Data Scale factor.
+            float maximumValue = 0f;
+            (float?[,,] allPointsData, _, _) = Data.GetAllPointsData();
+            foreach (float? point in allPointsData)
+            {
+                if (point.HasValue && Math.Abs(point.Value) > maximumValue)
+                {
+                    maximumValue = Math.Abs(point.Value);
+                }
+            }
+            header.AddRange(BitConverter.GetBytes(maximumValue / 32000f));
+            //9   uint16 Number of 512 - byte blocks to the Data Section + 1.
+            header.AddRange(BitConverter.GetBytes((UInt16)(blockLengthParameterSection + 1)));
+            //10  uint16 Analog Frames per Data Frame.
+            header.AddRange(BitConverter.GetBytes((UInt16)Required.Analog.SamplesPerFrame));
+            //11 - 12   float32     3D Point Data acquisition rate in Hertz.
+            header.AddRange(BitConverter.GetBytes(Required.Point.Rate * (float)Required.Analog.SamplesPerFrame));
+            //13 - 149  — 	Not used.
+            for (int i = 0; i < 137; i++)
+            {
+                header.AddRange(BitConverter.GetBytes((UInt16)0));
+            }
+            //150     uint16 Indicates support for 2 or 4 - character Header Event labels.
+            header.AddRange(BitConverter.GetBytes((UInt16)12345));
+            //151     uint16  Number of Header Events(0 - 18).
+            header.AddRange(BitConverter.GetBytes((UInt16)HeaderEvents.Length));
+            //152 	— 	Not used.
+            header.AddRange(BitConverter.GetBytes((UInt16)0));
+            //153 - 188     float32     Header Event times in seconds.
+            for (int i = 0; i < 18; i++)
+            {
+                if (i < HeaderEvents.Length)
+                {
+                    header.AddRange(BitConverter.GetBytes(HeaderEvents[i].EventTime));
+                }
+                else
+                {
+                    header.AddRange(BitConverter.GetBytes(0f));
+                }
+            }
+            //189 - 197     uint8   Header Event flag(0x00 = ON, 0x01 = OFF).
+            for (int i = 0; i < 18; i++)
+            {
+                if (i < HeaderEvents.Length)
+                {
+                    header.Add((byte)HeaderEvents[i].DisplayFlag);
+                }
+                else
+                {
+                    header.Add((byte)0x00);
+                }
+            }
+            //198 	— 	Not used.
+            header.AddRange(BitConverter.GetBytes((UInt16)0));
+            //199 - 234     ASCII   Header Event labels(2 or 4 characters, depending on Word 150).
+            for (int i = 0; i < 18; i++)
+            {
+                // If the header event support 4 character but the label is less
+                // We add two null character
+                if (i < HeaderEvents.Length)
+                {
+                    string label = HeaderEvents[i].EventLabel;
+                    if (label.Length > 4)
+                    {
+                        label = label.Substring(0, 4);
+                    }
+                    else if (label.Length < 4)
+                    {
+                        label = label.PadRight(4, '\0');
+                    }
+                    header.AddRange(System.Text.Encoding.ASCII.GetBytes(label));
+                }
+                // For the unused event we fill them with null character
+                else
+                {
+                    header.AddRange(System.Text.Encoding.ASCII.GetBytes("\0\0\0\0"));
+                }
+            }
+            //235 - 256 	— 	Not used.
+            for (int i = 0; i < 22; i++)
+            {
+                header.AddRange(BitConverter.GetBytes((UInt16)0));
+            }
+            return header.ToArray();
         }
 
         public byte[] ParametersToBinaries()
@@ -1139,6 +1273,16 @@ namespace SHARP3D
             return parametersBytes.ToArray();
         }
 
+        public void DataToBinaries(
+            float scaleFactor,
+            FileStream fs
+            )
+        {
+            //fs.WriteAsync(header);
+
+            
+        }
+
         public byte[] ParameterScalarToBinary(
             int idGroup,
             string name,
@@ -1798,114 +1942,6 @@ namespace SHARP3D
             return parameterBytes.ToArray();
         }
 
-        public byte[] DataToBinaries(float scaleFactor)
-        {
-            List<byte> data = new List<byte>();
-
-            return data.ToArray();
-        }
-
-        public byte[] HeaderToBinaries(int blockLengthParameterSection)
-        {
-            List<byte> header = new List<byte>();
-            //   Byte 1: uint8, Byte 2: char Byte 1: Number of 512 - byte blocks to Parameter Section +1.
-            header.Add((byte)0x02);
-            //    Byte 2: Data storage format flag.
-            header.Add((byte)0x50);
-            //2   uint16 Number of markers stored in each Data Frame.
-            header.AddRange(BitConverter.GetBytes((UInt16)Data.Points.Length));
-            //3   uint16 Total number of analog samples per Data Frame.
-            header.AddRange(BitConverter.GetBytes((UInt16)(Required.Analog.SamplesPerFrame * Data.Analogs.Length)));
-            //4   uint16 First frame number of raw data(not used / misleading).
-            header.AddRange(BitConverter.GetBytes((UInt16)1));
-            //5   uint16 Last frame number of raw data(not used / misleading).
-            header.AddRange(BitConverter.GetBytes((UInt16)Required.Point.Frames));
-            //6   uint16 Maximum 3D frame interpolation gap.
-            header.AddRange(BitConverter.GetBytes((UInt16)Required.Point.MaximumInterpolationGap));
-            //7 - 8     float32 Data Scale factor.
-            float maximumValue = 0f;
-            (float?[,,] allPointsData, _, _) = Data.GetAllPointsData();
-            foreach(float? point in allPointsData)
-            {
-                if (point.HasValue && Math.Abs(point.Value) > maximumValue)
-                {
-                    maximumValue = Math.Abs(point.Value);
-                }
-            }
-            header.AddRange(BitConverter.GetBytes(maximumValue / 32000f));
-            //9   uint16 Number of 512 - byte blocks to the Data Section + 1.
-            header.AddRange(BitConverter.GetBytes((UInt16)(blockLengthParameterSection + 1)));
-            //10  uint16 Analog Frames per Data Frame.
-            header.AddRange(BitConverter.GetBytes((UInt16)Required.Analog.SamplesPerFrame));
-            //11 - 12   float32     3D Point Data acquisition rate in Hertz.
-            header.AddRange(BitConverter.GetBytes(Required.Point.Rate * (float)Required.Analog.SamplesPerFrame));
-            //13 - 149  — 	Not used.
-            for(int i = 0; i < 137; i++)
-            {
-                header.AddRange(BitConverter.GetBytes((UInt16)0));
-            }
-            //150     uint16 Indicates support for 2 or 4 - character Header Event labels.
-            header.AddRange(BitConverter.GetBytes((UInt16)12345));
-            //151     uint16  Number of Header Events(0 - 18).
-            header.AddRange(BitConverter.GetBytes((UInt16)HeaderEvents.Length));
-            //152 	— 	Not used.
-            header.AddRange(BitConverter.GetBytes((UInt16)0));
-            //153 - 188     float32     Header Event times in seconds.
-            for (int i = 0; i < 18; i++)
-            {
-                if (i < HeaderEvents.Length)
-                {
-                    header.AddRange(BitConverter.GetBytes(HeaderEvents[i].EventTime));
-                }
-                else
-                {
-                    header.AddRange(BitConverter.GetBytes(0f));
-                }
-            }
-            //189 - 197     uint8   Header Event flag(0x00 = ON, 0x01 = OFF).
-            for (int i = 0; i < 18; i++)
-            {
-                if (i < HeaderEvents.Length)
-                {
-                    header.Add((byte)HeaderEvents[i].DisplayFlag);
-                }
-                else
-                {
-                    header.Add((byte)0x00);
-                }
-            }
-            //198 	— 	Not used.
-            header.AddRange(BitConverter.GetBytes((UInt16)0));
-            //199 - 234     ASCII   Header Event labels(2 or 4 characters, depending on Word 150).
-            for (int i = 0; i < 18; i++)
-            {
-                // If the header event support 4 character but the label is less
-                // We add two null character
-                if (i < HeaderEvents.Length)
-                {
-                    string label = HeaderEvents[i].EventLabel;
-                    if (label.Length > 4)
-                    {
-                        label = label.Substring(0, 4);
-                    }
-                    else if (label.Length < 4)
-                    {
-                        label = label.PadRight(4, '\0');
-                    }
-                    header.AddRange(System.Text.Encoding.ASCII.GetBytes(label));
-                }
-                // For the unused event we fill them with null character
-                else
-                {
-                    header.AddRange(System.Text.Encoding.ASCII.GetBytes("\0\0\0\0"));
-                }
-            }
-            //235 - 256 	— 	Not used.
-            for(int i = 0; i < 22; i++)
-            {
-                header.AddRange(BitConverter.GetBytes((UInt16)0));
-            }
-            return header.ToArray();
-        }
+        
     }
 }
